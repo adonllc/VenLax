130139dfecef3604d8567519946c4c24d9fa3cfa
Hemanth Palla <labscloudy@gmail.com>
2026-05-07 09:36:17 -0400

    feat(ui): complete web component library index exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,2 +1,94 @@
-export * from "./tokens";
-export * from "./theme";
+// Tokens + theme (existing)
+export { colors, typography, spacing, radius, getSurface } from './tokens';
+export type { Theme } from './tokens';
+export { ThemeProvider, useTheme } from './theme';
+
+// Atoms
+export { Button } from './components/atoms/Button';
+export type { ButtonProps, ButtonVariant, ButtonSize } from './components/atoms/Button';
+export { Input } from './components/atoms/Input';
+export type { InputProps } from './components/atoms/Input';
+export { Select } from './components/atoms/Select';
+export type { SelectProps, SelectOption } from './components/atoms/Select';
+export { Avatar } from './components/atoms/Avatar';
+export type { AvatarProps } from './components/atoms/Avatar';
+export { Badge } from './components/atoms/Badge';
+export type { BadgeProps, BadgeVariant } from './components/atoms/Badge';
+export { Spinner } from './components/atoms/Spinner';
+export type { SpinnerProps } from './components/atoms/Spinner';
+export { Toast } from './components/atoms/Toast';
+export type { ToastProps, ToastVariant } from './components/atoms/Toast';
+
+// Forecast
+export { ProbabilityBar } from './components/forecast/ProbabilityBar';
+export type { ProbabilityBarProps } from './components/forecast/ProbabilityBar';
+export { MarketStatusChip } from './components/forecast/MarketStatusChip';
+export type { MarketStatusChipProps, MarketStatus } from './components/forecast/MarketStatusChip';
+export { CategoryPill } from './components/forecast/CategoryPill';
+export type { CategoryPillProps, MarketCategory } from './components/forecast/CategoryPill';
+export { MarketCard } from './components/forecast/MarketCard';
+export type { MarketCardProps } from './components/forecast/MarketCard';
+export { PositionRow } from './components/forecast/PositionRow';
+export type { PositionRowProps } from './components/forecast/PositionRow';
+export { InsightCard } from './components/forecast/InsightCard';
+export type { InsightCardProps, InsightSignal } from './components/forecast/InsightCard';
+export { SettlementBanner } from './components/forecast/SettlementBanner';
+export type { SettlementBannerProps } from './components/forecast/SettlementBanner';
+export { ForecastEntryWidget } from './components/forecast/ForecastEntryWidget';
+export type { ForecastEntryWidgetProps } from './components/forecast/ForecastEntryWidget';
+
+// FP Economy
+export { FPBadge } from './components/fp-economy/FPBadge';
+export type { FPBadgeProps } from './components/fp-economy/FPBadge';
+export { StreakBanner } from './components/fp-economy/StreakBanner';
+export type { StreakBannerProps } from './components/fp-economy/StreakBanner';
+export { MissionCard } from './components/fp-economy/MissionCard';
+export type { MissionCardProps } from './components/fp-economy/MissionCard';
+export { BadgeDisplay } from './components/fp-economy/BadgeDisplay';
+export type { BadgeDisplayProps } from './components/fp-economy/BadgeDisplay';
+export { XPProgressBar } from './components/fp-economy/XPProgressBar';
+export type { XPProgressBarProps, XpLevel } from './components/fp-economy/XPProgressBar';
+export { RewardCard } from './components/fp-economy/RewardCard';
+export type { RewardCardProps } from './components/fp-economy/RewardCard';
+export { WalletSummary } from './components/fp-economy/WalletSummary';
+export type { WalletSummaryProps } from './components/fp-economy/WalletSummary';
+export { SubscriptionBadge } from './components/fp-economy/SubscriptionBadge';
+export type { SubscriptionBadgeProps, SubscriptionTier } from './components/fp-economy/SubscriptionBadge';
+
+// Reviews
+export { VerifiedBadge } from './components/reviews/VerifiedBadge';
+export type { VerifiedBadgeProps, ReviewBadge } from './components/reviews/VerifiedBadge';
+export { ReviewCard } from './components/reviews/ReviewCard';
+export type { ReviewCardProps } from './components/reviews/ReviewCard';
+export { HelpfulVoteRow } from './components/reviews/HelpfulVoteRow';
+export type { HelpfulVoteRowProps } from './components/reviews/HelpfulVoteRow';
+export { ReviewForm } from './components/reviews/ReviewForm';
+export type { ReviewFormProps, ReviewFormData } from './components/reviews/ReviewForm';
+export { TrustSummary } from './components/reviews/TrustSummary';
+export type { TrustSummaryProps } from './components/reviews/TrustSummary';
+
+// Social
+export { LeaderboardRow } from './components/social/LeaderboardRow';
+export type { LeaderboardRowProps } from './components/social/LeaderboardRow';
+export { ProfileHeader } from './components/social/ProfileHeader';
+export type { ProfileHeaderProps } from './components/social/ProfileHeader';
+export { AccuracyGauge } from './components/social/AccuracyGauge';
+export type { AccuracyGaugeProps } from './components/social/AccuracyGauge';
+export { FollowButton } from './components/social/FollowButton';
+export type { FollowButtonProps } from './components/social/FollowButton';
+export { ForecastHistoryRow } from './components/social/ForecastHistoryRow';
+export type { ForecastHistoryRowProps } from './components/social/ForecastHistoryRow';
+
+// Layout
+export { PageContainer } from './components/layout/PageContainer';
+export type { PageContainerProps } from './components/layout/PageContainer';
+export { EmptyState } from './components/layout/EmptyState';
+export type { EmptyStateProps } from './components/layout/EmptyState';
+export { NotificationBell } from './components/layout/NotificationBell';
+export type { NotificationBellProps } from './components/layout/NotificationBell';
+export { TopBar } from './components/layout/TopBar';
+export type { TopBarProps } from './components/layout/TopBar';
+export { Sidebar } from './components/layout/Sidebar';
+export type { SidebarProps, SidebarItem } from './components/layout/Sidebar';
+export { BottomTabBar } from './components/layout/BottomTabBar';
+export type { BottomTabBarProps, TabItem } from './components/layout/BottomTabBar';
